49e6ea3e7564a9b04b3e2a1c82956eaf61a3a1e1
kenLukau31 <kenlukau@gmail.com>
2026-01-19 21:48:04 +0000

    git merge origin/main

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="homepage"
+        name="index"
         options={{
           title: '',
           tabBarIcon: ({ focused }) => (
